e85edb6cdd6312777f6722121182b2c618e1a3e3
Kinga Haczela <haczelakinga@gmail.com>
2020-10-28 19:09:40 +0000

    Updates

---
+++ precache-manifest.90e018aedca95d0b3156a969cb18bcf4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0b071751ae06fcadf21d7057fd51cf0",
+    "url": "/money-manager-app/index.html"
+  },
+  {
+    "revision": "5ca9b983d4d4346b3398",
+    "url": "/money-manager-app/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "dd04d263b9857ddf4744",
+    "url": "/money-manager-app/static/css/main.82657cac.chunk.css"
+  },
+  {
+    "revision": "5ca9b983d4d4346b3398",
+    "url": "/money-manager-app/static/js/2.6dd3b981.chunk.js"
+  },
+  {
+    "revision": "fbbfe9ed4dc77af342a4a500830ec9a4",
+    "url": "/money-manager-app/static/js/2.6dd3b981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd04d263b9857ddf4744",
+    "url": "/money-manager-app/static/js/main.34d1f9ed.chunk.js"
+  },
+  {
+    "revision": "eabf7f83b28bac962e27",
+    "url": "/money-manager-app/static/js/runtime-main.3b2db223.js"
+  },
+  {
+    "revision": "712c9896c1a8eece1cf06056ce1e0505",
+    "url": "/money-manager-app/static/media/profilePic.712c9896.svg"
+  },
+  {
+    "revision": "2014fc42c4c8d0dfde1c8957d72f6d2d",
+    "url": "/money-manager-app/static/media/savingMoney.2014fc42.svg"
+  },
+  {
+    "revision": "0c511b230da62449f60ae9764e7b45de",
+    "url": "/money-manager-app/static/media/simpleApp.0c511b23.svg"
+  },
+  {
+    "revision": "b0faee39be57e29e5a5f40469ae46013",
+    "url": "/money-manager-app/static/media/statisticsImage.b0faee39.svg"
+  },
+  {
+    "revision": "8dbcb72d00cd0f18f35f4cae3ea38842",
+    "url": "/money-manager-app/static/media/undraw_blank_canvas_3rbb.8dbcb72d.svg"
+  },
+  {
+    "revision": "5baa9f5aed8583ca46944aa50bd8afba",
+    "url": "/money-manager-app/static/media/winner.5baa9f5a.svg"
+  }
+]);
