991a138d50c5dbd3d47394837f1fc67a051b2446
Kinga Haczela <haczelakinga@gmail.com>
2020-10-28 19:53:20 +0000

    Updates

---
+++ precache-manifest.a095f317b085b1889b98bb8af5673ed3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d300247fbcaf819ae31fcb1a07982acb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06af3dccef337b47ee5b",
+    "url": "/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "1f1cde7eb04670437954",
+    "url": "/static/css/main.82657cac.chunk.css"
+  },
+  {
+    "revision": "06af3dccef337b47ee5b",
+    "url": "/static/js/2.5048c671.chunk.js"
+  },
+  {
+    "revision": "fbbfe9ed4dc77af342a4a500830ec9a4",
+    "url": "/static/js/2.5048c671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f1cde7eb04670437954",
+    "url": "/static/js/main.c7739551.chunk.js"
+  },
+  {
+    "revision": "563543b1b297e705cfe7",
+    "url": "/static/js/runtime-main.ab609118.js"
+  },
+  {
+    "revision": "712c9896c1a8eece1cf06056ce1e0505",
+    "url": "/static/media/profilePic.712c9896.svg"
+  },
+  {
+    "revision": "2014fc42c4c8d0dfde1c8957d72f6d2d",
+    "url": "/static/media/savingMoney.2014fc42.svg"
+  },
+  {
+    "revision": "0c511b230da62449f60ae9764e7b45de",
+    "url": "/static/media/simpleApp.0c511b23.svg"
+  },
+  {
+    "revision": "b0faee39be57e29e5a5f40469ae46013",
+    "url": "/static/media/statisticsImage.b0faee39.svg"
+  },
+  {
+    "revision": "8dbcb72d00cd0f18f35f4cae3ea38842",
+    "url": "/static/media/undraw_blank_canvas_3rbb.8dbcb72d.svg"
+  },
+  {
+    "revision": "5baa9f5aed8583ca46944aa50bd8afba",
+    "url": "/static/media/winner.5baa9f5a.svg"
+  }
+]);
